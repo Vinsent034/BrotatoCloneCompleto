e0a838e06e79370e3bb7d5ec3e77fe98df10596e
Vincenzo Di Carluccio <vincenzo0144@gmail.com>
2025-10-24 23:43:42 +0200

    Delete Section 4 Brotato Clone/brotato-clone directory

--- Section 4 Brotato Clone/brotato-clone/scenes/arena/camera_2d.gd
+++
@@ -1,6 +0,0 @@
-extends Camera2D
-class_name Camera
-
-func _process(delta: float) -> void:
-	if is_instance_valid(Global.player):
-		global_position = Global.player.global_position
--- Section 4 Brotato Clone/brotato-clone/scenes/components/hurtbox_component.gd
+++
@@ -1,8 +0,0 @@
-extends Area2D
-class_name HurtboxComponent
-
-signal on_damaged(hitbox: HitboxComponent)
-
-func _on_area_entered(area: Area2D) -> void:
-	if area is HitboxComponent:
-		on_damaged.emit(area)
